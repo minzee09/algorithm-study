91a3df556c5ba66e7a2640338244129c8d2f2e97
Kim Minji <68258295+minzee09@users.noreply.github.com>
2024-04-12 11:38:12 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,7 +1,9 @@
-N = int(input())
-n_list = set(map(int, input().split()))
-M = int(input())
-m_list = list(map(int, input().split()))
+import sys
+
+N = int(sys.stdin.readline())
+n_list = set(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+m_list = list(map(int, sys.stdin.readline().split()))
 
 for i in range(M):
     if m_list[i] in n_list:
