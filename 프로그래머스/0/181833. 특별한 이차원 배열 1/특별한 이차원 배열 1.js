19f30ad1c9823c608757ec390b8eec57d289bdd4
Kim Minji <68258295+minzee09@users.noreply.github.com>
2024-12-27 02:42:12 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.26 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,11 +1,21 @@
+// function solution(n) {
+//     var answer = [[]];
+//     for(let i = 0; i < n;i++){
+//         if(i > 0) answer.push([]); //이게 왜 필요해? -> answer = [[]]는 이미 첫 번째 빈 배열(즉, 첫 번째 행)을 가지고 시작함. 따라서 첫 번째 행은 초기화 상태로 존재하고 나머지 행을 추가할 때만 push
+//         for(let j = 0; j < n;j++){
+//             if(i == j) answer[i].push(1)
+//             else answer[i].push(0)
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(n) {
-    var answer = [[]];
-    for(let i = 0; i < n;i++){
-        if(i > 0) answer.push([]); //이게 왜 필요해? -> answer = [[]]는 이미 첫 번째 빈 배열(즉, 첫 번째 행)을 가지고 시작함. 따라서 첫 번째 행은 초기화 상태로 존재하고 나머지 행을 추가할 때만 push
-        for(let j = 0; j < n;j++){
-            if(i == j) answer[i].push(1)
-            else answer[i].push(0)
-        }
-    }
-    return answer;
+  const answer = Array.from(Array(n), () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    answer[i][i] = 1;
+  }
+
+  return answer;
 }
