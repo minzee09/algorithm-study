16ffbbfdc63108e090b9f5978b67bc6c2ef6d0b3
Kim Minji <68258295+minzee09@users.noreply.github.com>
2025-01-02 19:07:24 +0900

    [level 0] Title: 글자 지우기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -0,0 +1,8 @@
+function solution(my_string, indices) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++){
+       if (indices.includes(i)) continue;
+        answer += my_string[i];
+    }
+    return answer;
+}
