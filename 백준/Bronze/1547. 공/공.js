0e75b7dd92fc303488bc76943cee1dd482c60e3b
Minji Kim <68258295+minzee09@users.noreply.github.com>
2025-07-15 17:32:27 +0900

    [Bronze III] Title: 공, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.js
+++ 백준/Bronze/1547. 공/공.js
@@ -1,21 +1,19 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin', 'utf8')
-  .trim()
-  .split(/\s+/)
-  .map(Number);
+// 컵 3개 순서대로 1번(공 O), 2번, 3번
+// 컵 2개를 고르고 서로 위치 바꾸고, 단 공의 위치는 바뀌지 않음
+// M <= 50
+// X,Y <= 3, X=Y일 수 있음
+
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split('\n');
 
 const M = input[0];
-let ball = 1;   // 처음에 1번 컵 아래에 공이 있음
-let idx = 1;
+let ball = 1;
 
-for (let i = 0; i < M; i++) {
-  const x = input[idx++];
-  const y = input[idx++];
-  if (ball === x) {
-    ball = y;
-  } else if (ball === y) {
-    ball = x;
-  }
+for (let i = 1; i <= M; i++) {
+    const [x, y] = input[i].split(' ').map(Number);
+    if (ball === x) {
+        ball = y;
+    } else if (ball === y) {
+        ball = x;
+    }
 }
-
 console.log(ball);
