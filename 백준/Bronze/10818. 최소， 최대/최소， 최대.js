f2f3a491fbcc7fdf0678dab2cb7a486483a9186c
Minji Kim <68258295+minzee09@users.noreply.github.com>
2025-08-08 01:19:31 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 95020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,4 +1,4 @@
 const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split('\n');
 
-const num = input[1].split(' ').map(Number).sort((a,b) => a-b);
-console.log(num[0],num[num.length-1]);
+const num = input[1].split(' ').map(Number);
+console.log(Math.min(...num),Math.max(...num));
