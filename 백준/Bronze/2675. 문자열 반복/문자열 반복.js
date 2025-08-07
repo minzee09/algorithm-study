79982887720459e466768d8897d219c8ffe82748
Minji Kim <68258295+minzee09@users.noreply.github.com>
2025-08-08 02:37:13 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split('\n');
+
+let answer = '';
+for(let i = 1; i <= input[0]; i++){
+    const repeat = input[i].split(' ')[0];
+    let str = input[i].split(' ')[1];
+    for (s of str){
+        answer += s.repeat(repeat);
+    }
+    answer += '\n'
+}
+console.log(answer.trim());
