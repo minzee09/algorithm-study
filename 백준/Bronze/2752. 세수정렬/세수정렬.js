3bd6b4502ce147d62c0af1758e67ec63d5c722ff
Minji Kim <68258295+minzee09@users.noreply.github.com>
2025-08-11 04:00:49 +0900

    [Bronze IV] Title: 세수정렬, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -1,11 +1,3 @@
-const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split(' ').map(Number);
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split(' ');
 
-const sortedNum = input.sort((a,b) => a - b);
-let answer = "";
-for(let i = 0; i < sortedNum.length; i++) {
-    answer += sortedNum[i] + ' ';
-}
-
-console.log(answer);
-
-// 그냥 sort()하면 문자열 기준으로 정렬이 되기 때문에 숫자가 2자리 이상되면 제대로 오름차순으로 정렬이 안됨. 따라서 a - b를 리턴해줘야함
+console.log(input.sort((a,b) => a - b).join(' '))
