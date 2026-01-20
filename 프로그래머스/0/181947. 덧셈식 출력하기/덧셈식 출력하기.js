82888a66eafc0105154ef1b77158b9108d5b8cac
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-21 00:40:10 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 308.51 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -7,10 +7,7 @@ const rl = readline.createInterface({
 let input = [];
 
 rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    let a = Number(input[0]);
-    let b = Number(input[1]);
+    const[a,b] = line.split(' ');
     
-    console.log( a,"+",b,"=",a+b);
+    console.log(a, "+", b, "=", Number(a) + Number(b));
 });
