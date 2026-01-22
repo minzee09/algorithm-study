847a9516c95be04876baed66a8c6281beb0d7656
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-23 00:39:01 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.83 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,14 +1,5 @@
 function solution(n, control) {
-    keys = {
-        'w':1,
-        's':-1,
-        'd':10,
-        'a':-10,
-    };
-   
-    for(let char of control){
-        n+=keys[char];
-    }
-    
+    let index = {w:1, s:-1, d:10, a:-10};
+    for(str of control) n += index[str];
     return n;
 }
