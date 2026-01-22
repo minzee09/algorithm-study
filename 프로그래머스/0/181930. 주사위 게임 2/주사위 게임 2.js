4e6513cd4787ddf588643a08dd55cba731825177
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-23 01:21:30 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,11 @@
 function solution(a, b, c) {
-    var answer = a + b + c;
+  const sum1 = a + b + c;
+  const sum2 = a ** 2 + b ** 2 + c ** 2;
+  const sum3 = a ** 3 + b ** 3 + c ** 3;
 
-    if (a === b && b === c) {
-        answer *= (a**2 + b**2 + c**2);
-        answer *= (a**3 + b**3 + c**3);
-    } else if (a === b || b === c || c === a) {
-        answer *= (a**2 + b**2 + c**2);
-    }
-    return answer;
+  const uniqueCount = new Set([a, b, c]).size;
+
+  if (uniqueCount === 3) return sum1;
+  if (uniqueCount === 2) return sum1 * sum2;
+  return sum1 * sum2 * sum3; 
 }
