5336e130dc86f514aad50cd7922a356aab998948
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-22 23:39:13 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,12 +1,9 @@
 function solution(n) {
-    var even = 0;
-    var odd = 0;
-    for (let i = 1; i <= n; i++){
-        if (n % 2 == 0 && i % 2 == 0){
-            even += i*i;
-        }else if (i % 2 == 1) odd += i;
-        
+    let isEven = (n%2 === 0) ? true : false;
+    var answer = 0;
+    for(let i = 1; i <= n; i+=2) {
+        if (n % 2 === 0) answer += (i+1) ** 2;
+        else answer += i;
     }
-    if (even > 0) return even;
-    return odd;
+    return answer;
 }
