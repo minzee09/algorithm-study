1ab6af6c8044c3a41a81cd3b7a53c5fe81c389b8
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-23 00:52:08 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,11 +1,3 @@
 function solution(num_list) {
-    var num1 = 1;
-    var num2 = 0;
-    
-    for (let i = 0; i < num_list.length; i++){
-        num1 *= num_list[i];
-        num2 += num_list[i];
-    }
-    if(num1 < num2*num2) return 1;
-    return 0;
+    return num_list.reduce((a, b) => a * b, 1) < (num_list.reduce((a, b) => a + b, 0) ** 2) ? 1 : 0;
 }
