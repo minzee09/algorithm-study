edd238adef2faaba4d79a48bf24477bfa9f95576
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-22 23:06:35 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,10 +1,7 @@
 function solution(str1, str2) {
-    var string1 = str1.split('');
-    var string2 = str2.split('');
-        var answer = [];
-    for(let i = 0; i < string1.length; i++){
-        answer.push(string1[i]+string2[i]);
+    var answer = '';
+    for (let i = 0; i < str1.length; i++) {
+        answer += str1[i] + str2[i];
     }
-
-    return answer.join('');
+    return answer;
 }
