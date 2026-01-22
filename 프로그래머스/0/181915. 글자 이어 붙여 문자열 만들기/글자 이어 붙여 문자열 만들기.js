5a0938535f58714671a746c7b44ea547befa876e
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-23 00:55:56 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,8 +1,5 @@
 function solution(my_string, index_list) {
     var answer = '';
-    for(let i = 0; i < index_list.length; i++){
-        num = index_list[i];
-        answer += my_string[num];
-    }
+    for(i of index_list) answer += my_string[i];
     return answer;
 }
