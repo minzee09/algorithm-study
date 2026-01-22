bdffd1bad978844d148a789e0749a65ac37b9652
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-23 00:21:02 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,4 +1,4 @@
 function solution(num_list) {
-    num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list.push(num_list[num_list.length - 1] - num_list[num_list.length - 2]) : num_list.push(num_list[num_list.length - 1] * 2)
-    return num_list;
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? (a-b):a*2];
 }
