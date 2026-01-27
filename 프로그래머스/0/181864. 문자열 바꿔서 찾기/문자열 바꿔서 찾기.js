786f03a633ddd61fdd4947d143c0885441732084
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-27 22:10:16 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,9 +1,6 @@
 function solution(myString, pat) {
-    myString = myString.split('');
-    for(let i in myString){
-        if (myString[i] === 'A') myString[i] = 'B';
-        else myString[i] = 'A'
-    }
-    myString = myString.join('')
-    return myString.includes(pat) ? 1 : 0;
+    myString = myString.replace(/A/g, "C");
+    myString = myString.replace(/B/g, "A");
+    myString = myString.replace(/C/g, "B");
+    return myString.includes(pat) ? 1 : 0 ;
 }
