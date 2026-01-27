4607a76ae7388af23dd099d1bbfc61fedd33d17a
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-27 22:35:49 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,6 +1,4 @@
 function solution(num_str) {
-    return num_str
-        .split('') // 문자열을 각 문자로 분리
-        .map(Number) // 각 문자를 숫자로 변환
-        .reduce((sum, num) => sum + num, 0); // 숫자를 모두 더함
+    var answer = num_str.split('');
+    return num_str.split('').reduce((a,b) => Number(a)+Number(b));
 }
