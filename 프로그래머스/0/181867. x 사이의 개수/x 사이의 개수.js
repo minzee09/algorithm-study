2c83b31b6d08b1f3c4c1de11a64f31753303f190
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-29 01:43:16 +0900

    [level 0] Title: x 사이의 개수, Time: 3.39 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,3 +1,13 @@
 function solution(myString) {
-    return myString.split('x').map(n => n.length);
+    var answer = [];
+    let count = 0;
+    for(let i = 0; i < myString.length; i++) {
+        if (myString[i] == 'x') {
+            answer.push(count);
+            count = 0;
+        }
+        else count++;
+    }
+    answer.push(count);
+    return answer;
 }
