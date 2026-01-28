1102ea34d78acad25183bced792bc66241a92760
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-29 01:09:10 +0900

    [level 0] Title: 배열 비교하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,8 +1,7 @@
 function solution(arr1, arr2) {
-    var answer = arr1.length < arr2.length ? -1 : 1;
-    if (arr1.length == arr2.length) {
-        if (arr1.reduce((a,b)=> a+b,0) == arr2.reduce((a,b)=> a+b,0)) return 0;
-        else answer = arr1.reduce((a,b)=> a+b,0) < arr2.reduce((a,b)=> a+b,0) ? -1 : 1;
+    if (arr1.length === arr2.length) {
+        if(arr1.reduce((a,b) => a+b) === arr2.reduce((a,b) => a+b)) return 0;
+        else return arr1.reduce((a,b) => a+b) > arr2.reduce((a,b) => a+b) ? 1 : -1;
     }
-    return answer;
+    return arr1.length > arr2.length ? 1 : -1;
 }
