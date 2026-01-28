db2500695299e412af93e0f545652a6889cca5dd
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-29 01:14:47 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,7 +1,6 @@
 function solution(a, b) {
     var answer = 0;
-    if(a % 2 && b % 2) return a**2 + b**2;
-    else if (a % 2 || b % 2) return 2 * (a + b);
-    else return Math.abs(a-b);;
-    
+    if (a % 2 === 1 && b % 2 === 1) return a**2 + b**2;
+    if (a % 2 === 0 && b % 2 === 0) return Math.abs(b-a);
+    return 2 * (a + b);
 }
