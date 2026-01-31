4b9d3aa65e1a91301a5d47afb789991a09348981
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-31 21:09:37 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,3 +1,4 @@
 function solution(myString, pat) {
-    return myString.toUpperCase().includes(pat.toUpperCase()) ? 1 : 0;
+    var answer = 0;
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
