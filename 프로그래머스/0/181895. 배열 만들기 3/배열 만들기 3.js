8db8f2f007b126e38fbf99970de01ce2780c3da9
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-01-31 21:14:02 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.68 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,6 +1,5 @@
 function solution(arr, intervals) {
-    var answer = [];
-    for(let i = 0; i < intervals.length;i++)
-    answer.push(...arr.slice(intervals[i][0],intervals[i][1]+1))
-    return answer;
+    var answer = arr.slice(intervals[0][0], intervals[0][1] + 1);
+    answer.push(arr.slice(intervals[1][0], intervals[1][1] + 1));
+    return answer.flat();
 }
