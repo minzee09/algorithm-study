3cec07374267f568407150c6bcf6b69be450a588
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-02-15 20:21:02 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,7 +1,3 @@
 function solution(names) {
-    var answer = [];
-    for (let i = 0; i < names.length; i += 5) {
-        answer.push(names[i]);
-    }
-    return answer;
+    return names.filter((_,i) => i % 5 == 0);
 }
