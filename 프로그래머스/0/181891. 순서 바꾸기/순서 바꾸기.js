13f1a397d6040fcf111a44920e3e0878e3b15478
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-02-15 20:15:12 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,6 +1,3 @@
 function solution(num_list, n) {
-    var answer = [];
-    answer.push(num_list.slice(n));
-    answer.push(num_list.slice(0, n));
-    return answer.flat();
+    return [...num_list.slice(n), ...num_list.slice(0, n)];
 }
