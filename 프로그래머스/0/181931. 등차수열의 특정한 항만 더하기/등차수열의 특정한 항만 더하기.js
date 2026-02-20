581c875707a26b93cd6fef83b60c4b6fbabd6ed9
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-02-21 01:47:40 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,11 +1,4 @@
 function solution(a, d, included) {
-    var answer = 0;
-    var sum = a;
-    for (let i = 0; i < included.length; i++){
-        if (included[i]){
-            answer += sum;
-        }
-        sum += d;
-    }
-    return answer;
+    included.map((n,idx) => n === true ? included[idx] = a + d*idx : included[idx] = 0);
+    return included.reduce((a,b) => a + b);
 }
