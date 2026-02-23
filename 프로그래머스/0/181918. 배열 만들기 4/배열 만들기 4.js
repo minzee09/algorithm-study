7cf233b350c82b820b6b0555c64c0b9415e0ca69
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-02-23 20:13:02 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.89 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,18 +1,16 @@
 function solution(arr) {
     var stk = [];
-    var i = 0;
-
-    while (i < arr.length) {
+    for(let i = 0; i < arr.length; i++) {
         if (stk.length === 0) {
             stk.push(arr[i]);
-            i++;
-        } else if (stk[stk.length - 1] < arr[i]) {
-            stk.push(arr[i]);
-            i++;
-        } else {
-            stk.pop();
+            continue;
         }
+        if (stk[stk.length-1] >= arr[i]) {
+            stk.pop();
+            i--;
+            continue;
+        };
+        stk.push(arr[i]);
     }
-
     return stk;
 }
