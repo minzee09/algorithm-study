b7ae1c639b7efd94e4a6366d9dba32008c23095f
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-04-03 11:37:26 +0900

    [level 2] Title: 소수 찾기, Time: 3.51 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -2,37 +2,36 @@ function solution(numbers) {
     const nums = numbers.split('');
     const visited = Array(nums.length).fill(false);
     const set = new Set();
-
-    // 소수 판별 함수
-    function isPrime(n) {
-        if (n < 2) return false; // 0과 1은 소수가 아닌 예외니까 제외
-        for (let i = 2; i <= Math.sqrt(n); i++) { // 약수 짝에서 작은 수 이미 판별했으니까 루트 n까지만 검사
-            if (n % i === 0) return false;
-        }
-        return true;
-    }
-
-    // 순열 생성
+    
     function dfs(current) {
         if (current.length > 0) {
             set.add(Number(current));
         }
-
-        for (let i = 0; i < nums.length; i++) {
-            if (!visited[i]) { // visited가 false면
+        for (let i = 0; i < nums.length; i++){
+            if(!visited[i]) {
                 visited[i] = true;
                 dfs(current + nums[i]);
                 visited[i] = false;
             }
         }
     }
-
+    
     dfs('');
+let answer = 0;
 
-    let count = 0;
-    set.forEach(num => {
-        if (isPrime(num)) count++;
-    });
+set.forEach((num) => {
+    if (num < 2) return;
 
-    return count;
+    let isPrime = true;
+
+    for (let j = 2; j <= Math.sqrt(num); j++) {
+        if (num % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) answer++;
+});
+    return answer;
 }
