d8b6047db4e7afe759154c4591243d381150f933
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-04-04 01:29:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.92 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,24 +1,15 @@
-// 1. participant 배열에서 각 선수 이름의 등장 횟수를 저장할 해시(=객체)를 만든다.
-// 2. completion 배열을 순회하면서 해당 선수의 등장 횟수를 해시에서 감소시킨다.
-// 3. 해시에서 값이 0보다 큰(==완주하지 못한) 선수 이름을 찾아 반환한다.
-
 function solution(participant, completion) {
+    const map = new Map();
     
-    // 1) 참가자들의 이름과 등장 횟수를 저장
-    const counts = {};
-    participant.forEach(name => {
-        counts[name] = (counts[name] || 0) + 1;
-    });
+    for (let p of participant) {
+        map.set(p, (map.get(p) || 0) + 1);
+    }
     
-    // 2) 완주자들의 이름에 해당하는 count 값을 감소
-    completion.forEach(name => {
-        counts[name]--;
-    });
-    
-    // 3) count가 0보다 큰 선수, 즉 완주하지 못한 선수를 반환
-    for (let name in counts) {
-        if (counts[name] > 0) {
-            return name;
-        }
+    for (let c of completion) {
+        map.set(c, map.get(c) - 1);
+    }
+
+    for (let [name, count] of map) {
+        if (count > 0) return name;
     }
 }
