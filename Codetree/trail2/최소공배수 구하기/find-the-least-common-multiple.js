ee1340e0d5ecacf5a59104cfbec9b635d5a827a1
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-06-01 23:59:01 +0900

    [Passed] 최소공배수 구하기 (69ms, 10MB)

---
+++ Codetree/trail2/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+function gcd(num1, num2){
+    let temp = 1;
+    for(let i = temp; i <= num1; i++){
+        if(num1 % i === 0 && num2 % i === 0) temp = i;
+    }
+    return temp;
+}
+
+let result = 0;
+
+if(n > m) result = gcd(m, n);
+else result = gcd(n,m);
+
+console.log((n * m) / result);
