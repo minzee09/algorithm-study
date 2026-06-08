7d9e8370bc7556ead7cbb1e9b97fe3fd455fb8bd
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-06-08 23:31:58 +0900

    [Passed] 두 수의 거듭제곱 (68ms, 10MB)

--- Codetree/trail2/두 수의 거듭제곱/two-numbers-of-squares.js
+++ Codetree/trail2/두 수의 거듭제곱/two-numbers-of-squares.js
@@ -2,5 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split(" ");
 const [a, b] = input.map(Number);
 // Please Write your code here. 
-
-console.log(a**b);
+function solution(a,b){
+    return a**b;
+}
+console.log(solution(a,b));
