a32eea783934642e18a144d25077f8215cc70ab9
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-06-21 01:42:34 +0900

    [Passed] 홀수 짝수에 따른 출력값 (66ms, 10MB)

---
+++ Codetree/trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+// Please Write your code here.
+function solution(n){
+    if(n%2 === 0) {
+        if (n === 2) return 2;
+    } else{
+        if(n===1) return 1;
+    }
+    return solution(n-2) + n;
+}
+
+console.log(solution(n));
