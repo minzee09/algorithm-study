8f38221b7761dc39db41fa82907da38824bd880e
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-06-25 22:43:24 +0900

    [Passed] k번째로 신기한 문자열 (76ms, 10MB)

---
+++ Codetree/trail2/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let index = 0;
+const [n, k, t] = input[index++].split(' ');
+const words = [];
+for (let i = 0; i < Number(n); i++) {
+    words.push(input[index++]);
+}
+// Please Write your code here.
+let filtered = words.filter(n => n.startsWith(t)).sort();
+console.log(filtered[k-1])
