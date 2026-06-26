cfe2fbd863af280b4361f493159db776ad5d4831
Minji Kim <68258295+minzee09@users.noreply.github.com>
2026-06-26 17:25:35 +0900

    [Passed] 비오는 날 (65ms, 10MB)

--- Codetree/trail2/비오는 날/rainy-day.js
+++ Codetree/trail2/비오는 날/rainy-day.js
@@ -2,51 +2,23 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const forecasts = input.slice(1, n + 1).map(line => line.split(' '));
 
-// Please Write your code here.
-class Forecast{
-    constructor(date, day, weather){
+class Forecast {
+    constructor(date, day, weather) {
         this.date = date;
         this.day = day;
         this.weather = weather;
     }
 }
 
-const forecast = [];
+let rainy = new Forecast("9999-99-99", "", "");
 
-for(let f of forecasts){
-   const [date, day, weather] = f;
-   forecast.push(new Forecast(date,day,weather));
-}
+for (let i = 1; i <= n; i++) {
+    const [date, day, weather] = input[i].split(' ');
+    const f = new Forecast(date, day, weather);
 
-const rainy = {
-    weather:'',
-    day:'',
-    date:'9999-99-99'
-}
-
-for(let f of forecast){
-    const [r_year, r_mon, r_day] = rainy.date.split('-');
-    const [year,mon,day] = f.date.split('-');
-    let needChange = false;
-
-    if(f.weather === 'Rain'){
-        if(r_year > year){
-            needChange = true;
-        } else if (r_year === year){
-            if(r_mon > mon){
-                needChange = true;
-            } else if (r_mon === mon){
-                if(r_day > day) needChange = true;
-            }
-        }
-    }
-
-    if(needChange){
-        rainy.date = f.date;
-        rainy.day = f.day;
-        rainy.weather = f.weather;
+    if (f.weather === "Rain" && f.date < rainy.date) {
+        rainy = f;
     }
 }
 
